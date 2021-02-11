23417a5b1956753e3a5141d13a222da4c0bc0529
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-02-11 14:31:04 +0100

    1 Versione

---
+++ ScuolaJPA-server/src/test/java/it/sirfin/ScuolaJPA/ScuolaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.ScuolaJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuolaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
